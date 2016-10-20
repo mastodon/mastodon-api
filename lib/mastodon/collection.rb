d74891add5a647a7481c25470602ed253f73a3ed
Eugen <eugen@zeonfederated.com>
2016-10-20 19:11:38 +0200

    Fix typo

--- lib/mastodon/collection.rb
+++ lib/mastodon/collection.rb
@@ -7,7 +7,7 @@ module Mastodon
     end
 
     def each(start = 0)
-      return to_enum(:each. start) unless block_given?
+      return to_enum(:each, start) unless block_given?
 
       Array(@collection[start..-1]).each do |element|
         yield(element)
