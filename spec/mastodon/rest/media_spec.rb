605778a4b9a8ad52adc19f5e47128724c1858b10
Colin Mitchell <muffinista@gmail.com>
2017-04-06 15:13:09 -0400

    Tweak media upload to support File and StringIO objects
    This commit should allow the client to upload File objects and/or
    StringIO objects without needing to directly instantiate a
    HTTP::FormData::File object.

--- spec/mastodon/rest/media_spec.rb
+++ spec/mastodon/rest/media_spec.rb
@@ -3,9 +3,26 @@ require 'spec_helper'
 describe Mastodon::REST::Media do
   before do
     @client = Mastodon::REST::Client.new(base_url: 'https://mastodon.social', bearer_token: '123456')
+    stub_request(:post, 'https://mastodon.social/api/v1/media').to_return(fixture('media.json'))
   end
 
   describe '#upload_media' do
-    pending
+    it 'works with a File' do
+      path = File.join(File.dirname(__FILE__), '..', '..', 'fixtures', 'upload.png')
+      media = @client.upload_media(File.new(path))
+      expect(media).to be_a Mastodon::Media
+    end
+
+    it 'works with a StringIO' do
+      media = @client.upload_media(StringIO.new)
+      expect(media).to be_a Mastodon::Media
+    end
+
+    it 'works with a HTTP::FormData::File' do
+      path = File.join(File.dirname(__FILE__), '..', '..', 'fixtures', 'upload.png')
+      f = HTTP::FormData::File.new(path)
+      media = @client.upload_media(f)
+      expect(media).to be_a Mastodon::Media
+    end
   end
 end
