9a4c7b8c05f052312cc90cbd202c8e8e7c1378ae
Colin Mitchell <muffinista@gmail.com>
2017-04-10 10:48:58 -0400

    Fix some rubocop/style issues

--- lib/mastodon/rest/request.rb
+++ lib/mastodon/rest/request.rb
@@ -19,10 +19,7 @@ module Mastodon
       def perform
         options_key = @request_method == :get ? :params : :form
         response    = http_client.headers(@headers).public_send(@request_method, @uri.to_s, options_key => @options)
-        if ENV["DEBUG"] == "true"
-          STDERR.puts response.inspect
-          STDERR.puts response.body
-        end
+        STDERR.puts response.body if ENV['DEBUG'] == 'true'
 
         fail_or_return(response.code, response.body.empty? ? '' : response.parse)
       end
