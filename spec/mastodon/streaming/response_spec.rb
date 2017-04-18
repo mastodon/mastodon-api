c2f8306f11ed03a6d2635173fbf83b348ba3d307
Colin Mitchell <muffinista@gmail.com>
2017-04-18 10:00:21 -0400

    Use fixture instead of hardcoded strings

--- spec/mastodon/streaming/response_spec.rb
+++ spec/mastodon/streaming/response_spec.rb
@@ -32,10 +32,12 @@ describe Mastodon::Streaming::Response do
     end
 
     it 'works with chunks' do
-      data1 = 'event: update
-data: {"id":29443,"created_at":"2017-04-14T18:05:28.000Z","in_reply_to_id":null,"in_reply_to_account_id":null,"sensitive":false,"spoiler_text":"","visibility":"public","application":null,"account":{"id":1217,"username":"hellojed","acct":"hellojed@mastodon.social","display_name":"hellojed","locked":false,"created_at":"2017-04-07T19:20:26.126Z","followers_count":1,"following_count":1,"statuses_count":201,"note":"Moped enthusiast and game developer in the Seattle area.\n\n<a href=\"https://hellojed.itch.io\" rel=\"nofollow noopener\"><span class=\"invisible\">https://</span><span class=\"\">hellojed.itch.io</span><span class=\"invisible\"></span></a>","url":"https://mastodon.social/@hellojed","avatar":"https://botsinspace.s3.amazonaws.com/accounts/avatars/000/001/217/original/'
-      data2 =
-        '5-1-16-3.png?1491592826","avatar_static":"https://botsinspace.s3.amazonaws.com/accounts/avatars/000/001/217/original/5-1-16-3.png?1491592826","header":"/headers/original/missing.png","header_static":"/headers/original/missing.png"},"media_attachments":[],"mentions":[],"tags":[],"uri":"tag:mastodon.social,2017-04-14:objectId=2548059:objectType=Status","content":"<p>this is a real NPM Package that http-server depends on <a href=\"https://www.npmjs.com/package/fahfahfajhfajhfajhfajhfajfaj\" rel=\"nofollow noopener\"><span class=\"invisible\">https://www.</span><span class=\"ellipsis\">npmjs.com/package/fahfahfajhfa</span><span class=\"invisible\">jhfajhfajhfajfaj</span></a></p>","url":"https://mastodon.social/users/hellojed/updates/1856764","reblogs_count":0,"favourites_count":0,"reblog":null}'
+      src = fixture('update_streaming.txt').read
+
+      # split into two random chunks
+      point = rand 5..src.length
+      data1 = src.slice(0..point)
+      data2 = src.slice((point + 1)..-1)
 
       subject.on_body(data1)
       expect(@last_type).to be_nil
